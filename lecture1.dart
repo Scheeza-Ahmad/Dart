6d130769d4c6ac07eaf9c9dc31f8d144d10e8329
Scheeza-Ahmad <scheezaahmad117@gmail.com>
2025-08-07 21:09:19 +0500

    Add files via upload

---
+++ lecture1.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+void main() {
+  ////DATATYPES
+  ///int,string,double,var,dynamic
+  print("We are studying datatypes!!!!");
+  String name = 'scheeza';
+  print('My name is : $name');
+  double marks = 99.9;
+  print(marks);
+  int age = 21;
+  print(age);
+  //Difference between var and dynamic is in var we cannot change the datatype of the variable but we can change the datatype of the variable in dynamic
+  var unchanged = "abc";
+  print(unchanged);
+  unchanged = "xyz";
+  print(unchanged);
+  // unchanged = 123; In var we can reallocatethe value but we cannot change the datatype of the variable
+  dynamic changeable = "abc";
+  print(changeable);
+  changeable = 123;
+  //runtimeType tells about the datatype of the varibale on runtime
+  print(changeable.runtimeType);
+
+  ////ARITHMATIC OPERATIONS
+  ///Addition subtraction multiplication and division
+  print("We are studying Arithmatic operations!!!!");
+
+  int num1 = 10;
+  int num2 = 12;
+  int sum = num1 + num2;
+  print(sum);
+
+  ////LOGICAL OPERATIONS
+  ///AND,OR,NOT,boolean
+  print("We are studying Logical Operations!!!!");
+  bool istrue = true;
+  print(istrue);
+  //input-->yeh simply run nhi hota isa run karna ka liya terminal main likhna hai dart run lecture1.dart
+  print("Enter your name : ");
+  String? n = stdin.readLineSync();
+  print("My name is :$n");
+}
